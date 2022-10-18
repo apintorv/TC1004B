31d22a415f75d30c09cabcc3a60d2763e68a3317
apintorv <a01067424@tec.mx>
2022-10-18 13:46:46 +0000

    18 oct

---
+++ popen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    FILE *sort;
+    char *cadenas[10];
+    cadenas[0] = "hola";
+    cadenas[1] = "otra";
+    cadenas[2] = "ya";
+    cadenas[3] = "alan";
+    cadenas[4] = "fin";
+
+    sort = popen("sort", "w");
+
+    for (int i =0; i < 4; i++){
+        fprintf(sort, "%s\n", cadenas[i]);
+    }
+
+    pclose(sort);
+    return 0;
+}
